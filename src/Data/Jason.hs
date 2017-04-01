a1886ad940fcc6cc5ab98e052840f47bbfe95162
Richard Lupton <richard.lupton@gmail.com>
2017-04-01 16:24:28 +0100

    Improving haddock documentation
    Preparing for upload to hackage, and improving the formatting of the documentation.

--- src/Data/Jason.hs
+++ src/Data/Jason.hs
@@ -1,13 +1,28 @@
+{-|
+Module      : Data.Jason
+Description : A library for automatically deriving JSON parsers from types
+Copyright   : (c) Richard Lupton, 2017
+License     : BSD-3
+Stability   : experimental
+Portability : POSIX
+-}
 {-# LANGUAGE DataKinds, GADTs, KindSignatures, TypeOperators #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 module Data.Jason (
-  JSBranch
-, unwrap
-, Parse
+  -- * Introduction
+  -- $example
+
+  -- * Building types
+  Parse
 , type( |>| )
-, List ) where
+, List
+, unwrap
+
+-- * Internal types
+, JSBranch ) where
+
 
 import           Data.Aeson ((.:))
 import qualified Data.Aeson as A
@@ -22,40 +37,36 @@ import           Data.Text (pack)
 import           Lib.Prelude
 
 
-{-|
-  A small (artificial) example demonstrating how to use the types defined here.
+-- $example
+-- A small (artificial) example demonstrating how to use the types defined here.
+--
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- > {-# LANGUAGE DataKinds #-}
+-- > {-# LANGUAGE TypeOperators #-}
+-- > import Data.Jason
+-- > import Data.Aeson (decode)
+-- > import Data.Text (Text)
+-- >
+-- > json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}" :: Text
+-- >
+-- > -- Extract [41, 42] inside the Jason types
+-- > parsed = decode json :: Maybe ("key1" |>| List ("key2" |>| Parse Integer))
+-- >
+-- > -- We can dispose of the types using unwrap: 'values' will have the value
+-- > -- Just [41, 42]
+-- > values :: Maybe [Integer]
+-- > values = fmap unwrap parsed
 
-  @
-    {-# LANGUAGE OverloadedStrings #-}
-    {-# LANGUAGE DataKinds #-}
-    {-# LANGUAGE TypeOperators #-}
-
-    import Data.Jason
-    import Data.Aeson (decode)
-    import Data.Text (Text)
-
-    json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}" :: Text
-
-    -- Extract [41, 42] inside the Jason types
-    parsed = decode json :: Maybe ("key1" |>| List ("key2" |>| Parse Integer))
-
-    -- We can dispose of the types using unwrap: 'values' will have the value
-    -- Just [41, 42]
-    values :: Maybe [Integer]
-    values = fmap unwrap parsed
-  @
-
--}
 
 
 --------------------------------------------------------------------------------
 -- Type level API using a type family to mirror JSON structure
 --------------------------------------------------------------------------------
 
--- | 'Parse a' represents trying to parse JSON to an 'a'.
+-- | @Parse a@ represents trying to parse JSON to an @a@.
 type Parse a = JSBranch '[] a
 
--- | The type operator '|>|' provides a nice way of describing how to walk
+-- | The type operator '|>|' provides a way of describing how to walk
 -- down a JSON tree.
 type family (x :: Symbol) |>| (b :: *) :: *
 type instance (x :: Symbol) |>| JSBranch xs a = JSBranch (x ': xs) a
@@ -71,8 +82,8 @@ type instance List (JSBranch xs a) = Parse [JSBranch xs a]
 --------------------------------------------------------------------------------
 
 -- | 'JSBranch' is a dependent datatype which represents a walk down a JSON
--- tree. 'JSBranch ["key1", "key2"] a' represents the walk "take the value at
--- 'key1' and then the value at 'key2', and (try to) interpret that as an 'a'".
+-- tree. @JSBranch ["key1", "key2"] a@ represents the walk "take the value at
+-- @key1@ and then the value at @key2@, and (try to) interpret that as an @a@".
 data JSBranch :: [Symbol] -> * -> * where
   JSNil :: a -> JSBranch '[] a
   JSCons :: JSBranch xs a -> JSBranch (t ': xs) a
