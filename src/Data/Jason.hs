b3d0bbca6dccb843db6900200ccbaade5eefd520
Richard Lupton <richard.lupton@gmail.com>
2017-04-01 13:44:55 +0100

    Extracted code from haskell-etcd
    Moved code across. Builds and passes tests.

--- src/Data/Jason.hs
+++ src/Data/Jason.hs
@@ -1,1 +1,67 @@
-module Data.Jason where
+{-# LANGUAGE DataKinds, GADTs, KindSignatures, TypeOperators #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+module Data.Jason (
+  JSBranch
+, unwrap
+, Parse
+, type( |>| )
+, List) where
+
+import           Data.Aeson ((.:))
+import qualified Data.Aeson as A
+import           Data.Aeson.Types (Parser)
+import           Data.Singletons (Sing, SingI(..))
+import           Data.Singletons.Prelude.List (Sing(SNil, SCons))
+import           Data.Singletons.TypeLits ( Symbol, SSymbol, KnownSymbol
+                                          , withKnownSymbol, symbolVal )
+import           Data.String (String)
+import           Data.Text (pack)
+
+import           Lib.Prelude
+
+
+data JSBranch :: [Symbol] -> * -> * where
+  JSNil :: a -> JSBranch '[] a
+  JSCons :: JSBranch xs a -> JSBranch (t ': xs) a
+
+
+unwrap :: JSBranch xs a -> a
+unwrap b = case b of
+  JSNil x -> x
+  JSCons b' -> unwrap b'
+
+
+instance (A.FromJSON a, SingI xs) => A.FromJSON (JSBranch xs a) where
+  parseJSON :: (A.FromJSON a, SingI xs) => A.Value -> Parser (JSBranch xs a)
+  parseJSON = parseSing sing
+    where
+      parseSing :: (A.FromJSON a) => Sing xs -> A.Value -> Parser (JSBranch xs a)
+      parseSing s o = case s of
+        SNil -> JSNil <$> A.parseJSON o
+        x `SCons` xs -> case o of
+          A.Object v -> let key = pack (reflectSym x) in
+            JSCons <$> (v .: key >>= parseSing xs)
+          _ -> empty
+
+
+reflectSym :: SSymbol s -> String
+reflectSym s = withKnownSymbol s $ proxySym s Proxy
+  where
+    proxySym :: (KnownSymbol n) => SSymbol n -> Proxy n -> String
+    proxySym _ = symbolVal
+
+
+--------------------------------------------------------------------------------
+-- Nicer type level API using a type family to mirror JSON structure
+--------------------------------------------------------------------------------
+
+type Parse a = JSBranch '[] a
+
+type family (x :: Symbol) |>| (b :: *) :: *
+type instance (x :: Symbol) |>| JSBranch xs a = JSBranch (x ': xs) a
+
+type family List (x :: *) :: *
+type instance List (JSBranch xs a) = Parse [JSBranch xs a]
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+import Test.Framework as TF
+import Test.Framework.Providers.HUnit (hUnitTestToTests)
+import Test.HUnit as HU
+import Test.HUnit ((~:),(@=?))
+
 import Protolude
+import Data.Jason
+
+import qualified Data.Aeson as A
+import Data.Text (Text)
 
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: Text)
+main = defaultMain tests
+
+tests :: [ TF.Test ]
+tests = [ genericJSONParserTests ]
+
+
+genericJSONParserTests :: TF.Test
+genericJSONParserTests = testGroup "\nGeneric JSON Parser tests" . hUnitTestToTests $
+  HU.TestList [ canExtractTextInJSBranch
+              , canExtractIntegerInJSBranch
+              , canExtractListInJSBranch
+              , canExtractListInJSBranchWithTypeFamily ]
+
+
+--------------------------------------------------------------------------------
+-- Generic JSON Parser tests
+--------------------------------------------------------------------------------
+
+canExtractTextInJSBranch :: HU.Test
+canExtractTextInJSBranch = "Can extract JSBranch on Text" ~:
+  let json = "{\"key1\":{\"key2\":{\"key3\":\"Some text\"}}}"
+      expected = Just "Some text"
+      decoded = A.decode json :: Maybe (JSBranch '["key1", "key2", "key3"] Text)
+  in
+    expected @=? fmap unwrap decoded
+
+canExtractIntegerInJSBranch :: HU.Test
+canExtractIntegerInJSBranch = "Can extract JSBranch on Integer" ~:
+  let json = "{\"key1\":{\"key2\":{\"key3\":42}}}"
+      expected = Just 42
+      decoded = A.decode json :: Maybe (JSBranch '["key1", "key2", "key3"] Integer)
+  in
+    expected @=? fmap unwrap decoded
+
+canExtractListInJSBranch :: HU.Test
+canExtractListInJSBranch = "Can extract JSBranch on List" ~:
+  let json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}"
+      expected = Just [41,42]
+      decoded = A.decode json :: Maybe (JSBranch '["key1"] [JSBranch '["key2"] Integer])
+  in
+    expected @=? (fmap (fmap unwrap) . (fmap unwrap) $ decoded)
+
+
+canExtractListInJSBranchWithTypeFamily :: HU.Test
+canExtractListInJSBranchWithTypeFamily = "Can extract JSBranch on List with type family" ~:
+  let json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}"
+      expected = Just [41,42]
+      decoded = A.decode json :: Maybe ("key1" |>| List ("key2" |>| Parse Integer))
+  in
+    expected @=? (fmap (fmap unwrap) . (fmap unwrap) $ decoded)
