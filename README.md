0d63fa1b95f63d67e34be922e9556f5779d14d58
Richard Lupton <richard.lupton@gmail.com>
2017-04-01 15:31:41 +0100

    Adding documentation
    Adding basic examples for how to use the library.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # jason
 
-add description of jason here
+`jason` is a dependently type JSON parsing library, that provides a quick way to create JSON parsers by deriving them from a type level description of the position of the value to be obtained.
+
+## Example
+
+```Haskell
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+import Data.Jason
+import Data.Aeson (decode)
+import Data.Text (Text)
+
+json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}" :: Text
+
+-- Extract [41, 42] inside the Jason types
+parsed = decode json :: Maybe ("key1" |>| List ("key2" |>| Parse Integer))
+
+-- We can dispose of the types using unwrap
+values :: Maybe [Integer]
+values = fmap unwrap parsed
+```
--- src/Data/Jason.hs
+++ src/Data/Jason.hs
@@ -7,7 +7,7 @@ module Data.Jason (
 , unwrap
 , Parse
 , type( |>| )
-, List) where
+, List ) where
 
 import           Data.Aeson ((.:))
 import qualified Data.Aeson as A
@@ -22,11 +22,63 @@ import           Data.Text (pack)
 import           Lib.Prelude
 
 
+{-|
+  A small (artificial) example demonstrating how to use the types defined here.
+
+  @
+    {-# LANGUAGE OverloadedStrings #-}
+    {-# LANGUAGE DataKinds #-}
+    {-# LANGUAGE TypeOperators #-}
+
+    import Data.Jason
+    import Data.Aeson (decode)
+    import Data.Text (Text)
+
+    json = "{\"key1\":[{\"key2\":41},{\"key2\":42}]}" :: Text
+
+    -- Extract [41, 42] inside the Jason types
+    parsed = decode json :: Maybe ("key1" |>| List ("key2" |>| Parse Integer))
+
+    -- We can dispose of the types using unwrap: 'values' will have the value
+    -- Just [41, 42]
+    values :: Maybe [Integer]
+    values = fmap unwrap parsed
+  @
+
+-}
+
+
+--------------------------------------------------------------------------------
+-- Type level API using a type family to mirror JSON structure
+--------------------------------------------------------------------------------
+
+-- | 'Parse a' represents trying to parse JSON to an 'a'.
+type Parse a = JSBranch '[] a
+
+-- | The type operator '|>|' provides a nice way of describing how to walk
+-- down a JSON tree.
+type family (x :: Symbol) |>| (b :: *) :: *
+type instance (x :: Symbol) |>| JSBranch xs a = JSBranch (x ': xs) a
+
+-- | The 'List' type operator constructs a parsing type for parsing
+-- a list of JSON objects.
+type family List (x :: *) :: *
+type instance List (JSBranch xs a) = Parse [JSBranch xs a]
+
+
+--------------------------------------------------------------------------------
+-- Basic dependent structure
+--------------------------------------------------------------------------------
+
+-- | 'JSBranch' is a dependent datatype which represents a walk down a JSON
+-- tree. 'JSBranch ["key1", "key2"] a' represents the walk "take the value at
+-- 'key1' and then the value at 'key2', and (try to) interpret that as an 'a'".
 data JSBranch :: [Symbol] -> * -> * where
   JSNil :: a -> JSBranch '[] a
   JSCons :: JSBranch xs a -> JSBranch (t ': xs) a
 
 
+-- | 'unwrap' unwraps a value from it's parsing type.
 unwrap :: JSBranch xs a -> a
 unwrap b = case b of
   JSNil x -> x
@@ -46,22 +98,11 @@ instance (A.FromJSON a, SingI xs) => A.FromJSON (JSBranch xs a) where
           _ -> empty
 
 
+-- | 'reflectSym' reflects a type level symbol into a value level string
 reflectSym :: SSymbol s -> String
 reflectSym s = withKnownSymbol s $ proxySym s Proxy
   where
     proxySym :: (KnownSymbol n) => SSymbol n -> Proxy n -> String
     proxySym _ = symbolVal
 
-
---------------------------------------------------------------------------------
--- Nicer type level API using a type family to mirror JSON structure
---------------------------------------------------------------------------------
-
-type Parse a = JSBranch '[] a
-
-type family (x :: Symbol) |>| (b :: *) :: *
-type instance (x :: Symbol) |>| JSBranch xs a = JSBranch (x ': xs) a
-
-type family List (x :: *) :: *
-type instance List (JSBranch xs a) = Parse [JSBranch xs a]
 
